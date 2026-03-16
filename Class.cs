70a7a9a8c46350c621558e99c5abedd5503a4ed2
emanuelaschiopu1-glitch <emanuela.schiopu1@student.usv.ro>
2026-03-16 22:56:22 +0200

    Add files via upload

---
+++ Class.cs
@@ -0,0 +1,70 @@
+﻿namespace ExempluClase
+{
+    public class Eveniment
+    {
+        // data membră privată
+        private string locatie;
+
+        // proprietăți auto-implemented
+        public string NumeEveniment { get; set; }
+        public string TipEveniment { get; set; }
+        public string Organizator { get; set; }
+        public DateTime DataEveniment { get; set; }
+        public int NrInvitati { get; set; }
+        public double Buget { get; set; }
+
+        // constructor fără parametri
+        public Eveniment()
+        {
+            NumeEveniment = string.Empty;
+            TipEveniment = string.Empty;
+            Organizator = string.Empty;
+            locatie = string.Empty;
+            NrInvitati = 0;
+            Buget = 0;
+        }
+
+        // constructor cu parametri
+        public Eveniment(string nume, string tip, string organizator, DateTime data, string loc, int invitati, double buget)
+        {
+            NumeEveniment = nume;
+            TipEveniment = tip;
+            Organizator = organizator;
+            DataEveniment = data;
+            locatie = loc;
+            NrInvitati = invitati;
+            Buget = buget;
+        }
+
+        // metode set/get pentru locatie
+        public void SetLocatie(string loc)
+        {
+            locatie = loc;
+        }
+
+        public string GetLocatie()
+        {
+            return locatie;
+        }
+
+        // proprietate computed
+        public bool EsteEvenimentMare
+        {
+            get
+            {
+                return NrInvitati >= 100;
+            }
+        }
+
+        // metoda care returneaza informatiile
+        public string Info()
+        {
+            if (string.IsNullOrEmpty(NumeEveniment))
+            {
+                return "Eveniment nesetat";
+            }
+
+            return $"Eveniment: {NumeEveniment}, Tip: {TipEveniment}, Organizator: {Organizator}, Data: {DataEveniment.ToShortDateString()}, Locatie: {locatie}, Invitati: {NrInvitati}, Buget: {Buget}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ExempluClase
+{
+    class Program
+    {
+        static void Main()
+        {
+            // instantiere obiect fara parametri
+            var eveniment1 = new Eveniment();
+
+            string ev1 = eveniment1.Info();
+            Console.WriteLine(ev1);
+
+            Console.WriteLine($"Este eveniment mare? {eveniment1.EsteEvenimentMare}");
+
+            // instantiere obiect cu parametri
+            Eveniment eveniment2 = new Eveniment(
+                "Nunta Maria si Andrei",
+                "Nunta",
+                "Maria Popescu",
+                new DateTime(2026, 6, 20),
+                "Restaurant Royal",
+                150,
+                20000
+            );
+
+            string ev2 = eveniment2.Info();
+            Console.WriteLine(ev2);
+
+            Console.WriteLine($"Este eveniment mare? {eveniment2.EsteEvenimentMare}");
+
+            Console.ReadKey();
+        }
+    }
+}
